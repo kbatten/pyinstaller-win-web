1b0024dbbd601eca98c609ab66cea7778bb909eb
Keith Batten <kbatten@gmail.com>
2013-02-18 00:48:28 -0800

    remove dummy handleDrop

--- piww_website/static/workspace/workspace.js
+++ piww_website/static/workspace/workspace.js
@@ -1,13 +1,3 @@
-function handleDrop(event) {
-    event.stopPropagation();
-    event.preventDefault();
-
-    // if we drag over the image, we get a child node
-    var id = event.target.id || (event.target.parentNode && event.target.parentNode.id)
-
-    $("#debug").append("upload: " + event.originalEvent.dataTransfer.files[0].name + "(" + id + ")" + "<b>");
-}
-
 function handleLockClick(event) {
     // default state of unlocked
     if (typeof event.target.state === 'undefined') {
