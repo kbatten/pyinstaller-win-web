cb4d50d9e57714b590b49f1a7ecdadcf007444d6
Keith Batten <kbatten@gmail.com>
2013-03-03 23:04:13 -0800

    refactor upload without compression

--- piww_website/piww.py
+++ piww_website/piww.py
@@ -6,14 +6,13 @@ A web interface to pyinstaller running under wine.
 import uuid
 import re
 import base64
-import struct
-import zlib
 import zipfile
 import logging
 import os
 
 from sh import cd, mkdir, rm, pyinstaller_win
 from flask import Flask, render_template, redirect, url_for, request, Response, send_from_directory
+from werkzeug import secure_filename
 
 import fastlz
 
@@ -79,46 +78,32 @@ def download(fileid, filename):
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    filename = request.files['Filedata'].filename
-    body = request.files['Filedata'].read()
+    f = request.files['file']
+
+    filename = secure_filename(f.filename)
+
+    print filename
 
     pattern = '^([^<>:"/\|?*]+)\.py$'
     m = re.search(pattern, filename)
     if not m:
         logging.error("upload filename error")
         return "upload filename error"
+
     filebase = m.group(1)
     filename_py = filebase+'.py'
     filename_exe = filebase+'.exe'
 
     logging.info('processing: ' + filebase)
 
-    len_wire = len(body)
-    len_data = 0
-
-    decoded = base64.b64decode(body)
-    size = struct.unpack(">I", decoded[:4])[0]
-    checksum = struct.unpack(">I", decoded[4:8])[0]
-    # zlib.adler32 returns a signed checksum
-    if checksum > 2147483647:
-        checksum -= 4294967296
-    compressed = decoded[8:]
-    uncompressed = fastlz.decompress(compressed, size)
-    if zlib.adler32(uncompressed) != checksum:
-        raise IndexError ("checksum error")
-    len_data += len(uncompressed)
-
-    if len_data:
-        logging.info('compression: {len_wire}/{len_data} = {compression}%'.format(len_wire=len_wire, len_data=len_data, compression=int(100.0 * float(len_wire)/float(len_data))))
-    else:
-        logging.info('compression: {len_wire}/{len_data} = {compression}%'.format(len_wire=len_wire, len_data=len_data, compression='inf'))
-        
     fileid = base64.urlsafe_b64encode(uuid.uuid4().bytes).rstrip('=')
+
     cd('/tmp')
     mkdir('piww_'+fileid)
     cd('piww_'+fileid)
-    with file('/tmp/piww_'+fileid+'/'+filename_py, 'w') as f:
-        f.write(uncompressed)
+
+    f.save('/tmp/piww_'+fileid+'/'+filename_py)
+
     # pyinstaller-win is a wrapper to wine running python.exe pyinstaller
     #  it takes a python file as a parameter and outputs an exe file in 'dist'
     pyinstaller_win(filename_py)
--- piww_website/static/workspace/upload.js
+++ piww_website/static/workspace/upload.js
@@ -1,192 +1,116 @@
-/*global FastLz*/
+/*global $, console*/
 /*jslint browser: true*/
 /*jslint plusplus: true, vars: true */
 
-var e = function (id) {
+
+// https://github.com/dciccale/jslibraryboilerplate/blob/master/jslibraryboilerplate_vanilla.js
+(function (window) {
     "use strict";
-    return document.getElementById(id);
-};
 
-// only allow one file to be compressed/uploaded at a time
-var g_uploading = false;
-var g_compressStart = 0;
-
-// not a real event
-function compressionUpdate(evt) {
-    "use strict";
-    e('debug').innerHTML = Math.ceil(50 * (evt.loaded / evt.total)) + "%";
-    if (evt.loaded === evt.total) {
-        e('debug').innerHTML += "seconds to compress: " + (((new Date()).getTime() - g_compressStart) / 1000) + "<br>";
-    }
-}
-
-function processing(dots) {
-    "use strict";
-    var da = ["", ".", "..", "...", "..", "."];
-    dots = dots % 6;
-    e('debug').innerHTML = da[dots] + "processing" + da[dots];
-    setTimeout("processing(" + (dots + 1) + ")", 1000);
-}
-
-function uploadUpdate(evt) {
-    "use strict";
-    if (evt.target.readyState === 4) {
-        if (evt.target.status === 200) {
-            processing(0);
-            window.location.href = '/download/' + evt.target.responseText + '/package.zip';
-        }
-    }
-
-    if (evt.total === evt.loaded) {
-        e('debug').innerHTML = "100%";
-        //      g_uploading = false;
-    } else {
-        e('debug').innerHTML = 50 + Math.ceil(50 * (evt.loaded / evt.total)) + "%";
-    }
-}
-
-// asynchronously compress and then upload data
-// each compressed data chunk will be in its own mime part
-// callbacks will trigger on for each chunk that is processed with
-//   evt.loaded = bytes processed
-//   evt.total = total bytes to be processed
-// data is compressed with zlib deflate, then base64 encoded
-function uploadCompressedData(data, filename, compressionCallback, uploadCallback) {
-    "use strict";
-    // modified from https://developer.mozilla.org/en/using_files_from_web_applications
-
-    var c_compressedData = [];
-    var c_evt = [];
-    var c_data = data; // are we duplicating data?
-    var c_chunkSize = 1048576;
-
-    function uploadDataAsync() {
-        var dataSize = 0;
-        var boundary = "yyyyyyyyyyyyyyyyyyyy";
-        var uri = "/upload";
-        var xhr = new XMLHttpRequest();
-
-        // build up a fake Mime message, lots of parts to make sure server side can handle it
-        // server should only care about the Filedata part
-        var body = "--" + boundary + "\r\n";
-
-        var i;
-
-        for (i = 0; i < c_compressedData.length; i++) {
-            var chunk = window.btoa(c_compressedData[i]);
-            dataSize += chunk.length;
-
-            body += "--" + boundary + "\r\n";
-            body += "Content-Disposition: form-data; name=\"Filedata\"; filename=\"" + filename + "\"\r\n";
-            body += "Content-Type: application/octet-stream\r\n\r\n";
-            body += chunk + "\r\n";
-        }
-
-        body += "--" + boundary + "--";
-
-        xhr.open("POST", uri, true);
-        xhr.setRequestHeader("Content-Type", "multipart/form-data; boundary=" + boundary);
-
-        xhr.upload.addEventListener("progress", uploadCallback, false);
-        xhr.upload.addEventListener("load", uploadCallback, false);
-        //      xhr.upload.addEventListener("readystatechange", uploadCallback, false);
-        xhr.onreadystatechange = uploadCallback;
-
-        e('debug').innerHTML += "compressed chunks: " + c_compressedData.length + "<br>";
-        e('debug').innerHTML += "compressed size: " + body.length + "<br>";
-
-        xhr.send(body);
-    }
-
-    function int32ToBytes(int32) {
-        var bytes = [];
-        /*jslint bitwise: true*/
-        bytes[3] = String.fromCharCode(int32 & 255);
-        int32 = int32 >> 8;
-        bytes[2] = String.fromCharCode(int32 & 255);
-        int32 = int32 >> 8;
-        bytes[1] = String.fromCharCode(int32 & 255);
-        int32 = int32 >> 8;
-        bytes[0] = String.fromCharCode(int32 & 255);
-        /*jslint bitwise: false*/
-        return bytes.join("");
-    }
-
-    // adler32 is amazingly simple
-    function adler32(data) {
-        var a = 1;
-        var b = 0;
-        var i;
-        for (i = 0; i < data.length; i++) {
-            var ascii = data.charCodeAt(i);
-            a = (a + ascii) % 65521;
-            b = (a + b) % 65521;
-        }
-
-        return int32ToBytes(b * 65536 + a);
-    }
-
-    function compressDataChunkFastlz(chunk) {
-        // compress with fastlz
-        // need to compare how different chunk sizes work
-        var size = int32ToBytes(chunk.length);
-        var checksum = adler32(chunk);
-        var compressed = FastLz.compressor(chunk);
-        return size + checksum + compressed;
-    }
-
-    var compressDataChunk = compressDataChunkFastlz;
-
-    function compressDataAsync() {
-        // compress a data chunk
-        c_compressedData.push(compressDataChunk(c_data.slice(c_evt.loaded, c_evt.loaded + c_chunkSize)));
-        c_evt.loaded += c_chunkSize;
-        if (c_evt.loaded > c_evt.total) {
-            c_evt.loaded = c_evt.total;
-        }
-        compressionCallback(c_evt);
-        if (c_evt.loaded < c_evt.total) {
-            setTimeout(compressDataAsync, 0);
-        } else {
-            // compression is done, lets trigger upload
-            c_evt.loaded = 0;
-            c_evt.total = c_compressedData.length;
-            setTimeout(uploadDataAsync, 0);
-        }
-    }
-
-    c_evt.loaded = 0;
-    c_evt.total = c_data.length;
-
-    g_compressStart = (new Date()).getTime();
-
-    setTimeout(compressDataAsync, 0);
-}
-
-function handleFileSelect(evt) {
-    "use strict";
-    evt.stopPropagation();
-    evt.preventDefault();
-
-    if (g_uploading === true) {
-        return;
-    }
-    g_uploading = true;
-
-    var dataTransfer = evt.dataTransfer || (evt.originalEvent && evt.originalEvent.dataTransfer);
-
-    var f = dataTransfer.files[0]; // Grab just the first file as a File object
-    var reader = new window.FileReader();
-
-    reader.onload = function (e) {
-        uploadCompressedData(e.target.result, f.name, compressionUpdate, uploadUpdate);
+    var Uploader = function (url, filter) {
+        // auto-create new instance without the 'new' keyword
+        return new Uploader.prototype.init(url, filter);
     };
 
-    reader.readAsText(f);
-}
+    Uploader.prototype = {
+        constructor: Uploader,
 
-function handleDragOver(evt) {
-    "use strict";
-    evt.stopPropagation();
-    evt.preventDefault();
-}
+        init : function (url, filter) {
+            this.url = url;
+            this.filter = filter;
+
+            return this;
+        },
+
+        monitor_drop : function (selector) {
+            $(selector).bind("dragover", false);
+            var that = this;
+            $(selector).bind("drop", function (evt) {
+                evt.stopPropagation();
+                evt.preventDefault();
+
+                if (that.stage !== 0 || that.monitor_paused) {
+                    return;
+                }
+
+                // uploading
+                that.stage = 1;
+
+                var dataTransfer = evt.dataTransfer || (evt.originalEvent && evt.originalEvent.dataTransfer);
+                var f = dataTransfer.files[0]; // Grab just the first file as a File object
+                var reader = new window.FileReader();
+
+                // when file is done loading upload it
+                reader.onload = function (e) {
+                    var uri = "/upload";
+
+                    var boundary = "----multipartFormBoundary" + Math.random();
+                    var xhr = new XMLHttpRequest();
+                    xhr.open("POST", uri, true);
+                    xhr.setRequestHeader("Content-Type", "multipart/form-data; boundary=" + boundary);
+
+                    if (that.callback_success) {
+                        xhr.onreadystatechange = function (evt) {
+                            if (evt.target.readyState === 4) {
+                                if (evt.target.status === 200) {
+                                    that.callback_success(evt.target.responseText);
+                                }
+                            }
+                        };
+                    }
+                    if (that.callback_progress) {
+                        xhr.onprogress = function (evt) {
+                            if (evt.lengthComputable) {
+                                that.callback_progress(evt.loaded, evt.total);
+                            }
+                        };
+                    }
+
+                    var data = "--" + boundary + "\r\n";
+                    data += "Content-Disposition: form-data; name=\"file\"; filename=\"" + f.name + "\"\r\n";
+                    data += "Content-Type: application/octet-stream\r\n";
+                    data += e.target.result + "\r\n";
+                    data += "--" + boundary + "--";
+
+                    xhr.send(data);
+
+                    // finished uploading, reset
+                    that.stage = 0;
+                };
+                reader.readAsText(f);
+            });
+        },
+
+        success : function (callback_success) {
+            this.callback_success = callback_success;
+        },
+
+        progress : function (callback_progress) {
+            this.callback_progress = callback_progress;
+        },
+
+        toggle_monitor : function (state) {
+            if (state === undefined) {
+                // toggle
+                this.monitor_paused = this.monitor_paused ? false : true;
+            } else {
+                this.monitor_paused = state ? true : false;
+            }
+        },
+
+        url: "",
+        filter: [],
+        stage: 0,
+        percent: 0,
+        monitor_paused: false,
+        callback_success: undefined,
+        callback_progress: undefined
+    };
+
+  // the init method uses Wrangler prototype and constructor
+    Uploader.prototype.init.prototype = Uploader.prototype;
+    Uploader.prototype.init.prototype.constructor = Uploader;
+
+     // expose to global object
+    window.Uploader = Uploader;
+}(window));
